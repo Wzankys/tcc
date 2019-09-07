387be1cfc0c5779ca3c77aefd732e20976867636
Wzank <51346092+Wzank@users.noreply.github.com>
2019-09-07 11:21:31 -0300

    Initial commit

---
+++ teste/Assets/Scripts/EspecialIchigo.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EspecialIchigo : MonoBehaviour
+{
+    public Animator animator;
+    bool anim0 = false;
+    bool anim1 = false;
+
+    float timeAnim = 0.0f;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z)&& Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Deu boa");
+            carregarRea();
+            anim0 = true;
+        }
+        if (anim0&&anim1)
+        {
+            timeAnim += Time.deltaTime;
+            Debug.Log(timeAnim);
+            if(timeAnim >3)
+            {
+                carregarRea2();
+            }
+        }
+    }
+    void carregarRea()
+    {
+        animator.SetBool("AtkEspecial", true);
+    }
+    void carregarRea1()
+    {
+        animator.SetBool("AtkEspecial", false);
+        animator.SetBool("TrocaEP1", true);
+        anim1 =true;    }
+    void carregarRea2()
+    {
+        animator.SetBool("TrocaEP1", false);
+        animator.SetBool("TrocaEP2", true);
+    }
+    void fimAnim()
+    {
+        animator.SetBool("TrocaEP2", false);
+        timeAnim = 0;
+        anim0 = false;
+        anim1 = false;
+    }
+}
---
+++ teste/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float dashDist = 0;
+
+    private int dire;
+
+    Rigidbody2D rb;
+    public float runSpeed = 40f;
+    bool jump = false;
+    int contSeta = 0;
+    float timeDash = 0.3f;
+    float horizontalMove = 0f;
+    Vector3 sideDash;
+
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    /*void Dash(int lado)
+    {
+        
+        if (contSeta >= 2 && timeDash >0 && lado ==1)
+        {
+            //Debug.Log(" Its Works my little friends");
+            
+            animator.SetBool("isDash", true);
+            contSeta = 0;
+        }
+        else
+        {
+            timeDash = 0.5f;
+            contSeta++;
+        }
+    }*/
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal")*runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("isJumping", true);
+        }
+       if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            dire = 2;
+            if (contSeta >= 2 && timeDash > 0 && dire==2)
+            {
+                rb.AddForce( transform.TransformDirection(Vector2.right)*dashDist,ForceMode2D.Impulse);
+                animator.SetBool("isDash", true);
+                contSeta = 0;
+            }
+            else
+            {
+                timeDash = 0.3f;
+                contSeta++;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            dire = 1;
+            if (contSeta >= 2 && timeDash > 0&&dire==1)
+            {
+                rb.velocity =  Vector2.left * dashDist;
+                animator.SetBool("isDash", true);
+                contSeta = 0;
+            }
+            else
+            {
+                timeDash = 0.3f;
+                contSeta++;
+            }
+        }
+        if (timeDash>0)
+        {
+            timeDash -= 0.9f* Time.deltaTime;
+        }
+        else
+        {
+            timeDash = 0;
+        }
+   
+    }
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
+        
+    }
+    void Dash()
+    {
+        animator.SetBool("isDash", false);
+    }
+
+     void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false,jump);
+        jump = false;
+    }
+}
