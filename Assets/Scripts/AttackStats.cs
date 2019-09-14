65d0de22f02b3c1a5498646e40e4ef183999cf3e
Yan Jardim <yan.guilherme72@gmail.com>
2019-09-14 16:44:40 -0300

    Adicionado classe de ataque

---
+++ Assets/Scripts/AttackStats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "AttackStat", menuName = "Attack/Attack Stats", order = 1)]
+public class AttackStats : ScriptableObject {
+	public float damage;
+	public float duration;
+	public float cooldown;
+	public bool invunerable;
+	public string animation;
+	public string keyAxis;
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+	public AttackStats attackStats;
+	private Animator animator;
+	private float cooldown;
+	private bool canAttack = true;
+	// Start is called before the first frame update
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!attackStats) return;
+		Attack ();
+	}
+
+	void Attack () {
+		var axis = Input.GetAxisRaw (attackStats.keyAxis);
+		if (axis > 0 && canAttack) {
+			Debug.Log ("Teste");
+			animator.SetTrigger (attackStats.animation);
+			StartCoroutine (CooldownCoroutine ());
+		}
+	}
+
+	IEnumerator CooldownCoroutine () {
+		canAttack = false;
+		yield return new WaitForSeconds (attackStats.cooldown);
+		canAttack = true;
+	}
+}
