1e6e8d27fc70ec76ecc1b6f3c4575e57739877a4
Wzank <51346092+Wzank@users.noreply.github.com>
2019-11-03 02:10:41 -0200

    SFX incluidos mas não 100%
    Ambos os personagens com os sfx básicos, porém, alguns sfx possuem delay, acho que seja o caso de antecipar o começo de alguns sfx pois todos eles começam com alguns milésimos de segundo mudo.

---
+++ Assets/Scripts/Player/AudioList.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioList : MonoBehaviour
+{
+    public AudioClip sPresentation, sWin, sDash, sJump, sSelect, sAttack1, sAttack2, sAttack3, sHit1, sDeath;
+    private AudioSource audioS;
+     void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+    }
+
+   public void PlaySong(AudioClip clip)
+    {
+        audioS.clip = clip;
+        audioS.Play();
+    }
+    void jumpSound()
+    {
+        PlaySong(sJump);
+    }
+    void selectSound()
+    {
+        PlaySong(sSelect);
+    }
+    void attack1Sound()
+    {
+        PlaySong(sAttack1);
+    }
+    void attack2Sound()
+    {
+        PlaySong(sAttack2);
+    }
+    void attack3Sound()
+    {
+        PlaySong(sAttack3);
+    }
+    void hitSound()
+    {
+        PlaySong(sHit1);
+    }
+    void deathSound()
+    {
+        PlaySong(sDeath);
+    }
+    void winSound()
+    {
+        PlaySong(sWin);
+    }
+    void presentationSound()
+    {
+        PlaySong(sPresentation);
+    }
+    void dashSound()
+    {
+        PlaySong(sDash);
+    }
+}
